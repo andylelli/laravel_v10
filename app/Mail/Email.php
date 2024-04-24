94a9b5c4d1dc1d7cb1988506cbebe55e142bc5ca
Andy Lelli <andylelli@yahoo.com>
2024-04-24 21:28:42 +0100

    commit

--- app/Mail/Email.php
+++ app/Mail/Email.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class Mail extends Mailable
+class Email extends Mailable
 {
     use Queueable, SerializesModels;
     public $mailData;
