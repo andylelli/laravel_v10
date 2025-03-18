b4708628444eef69e03590c4de84eecf64f3cecf
Andy Lelli <andylelli@yahoo.com>
2025-03-18 17:26:44 +0000

    comit

--- routes/api.php
+++ routes/api.php
@@ -35,6 +35,11 @@ use App\Http\Controllers\GetLogController;
     //Route::middleware(['throttle:100,1'])->group(function(){
     //})
 
+        //Web
+        Route::get('/web/', function () {
+            return view('welcome');
+        });
+
         //Login
         Route::post('/post/login',  [PostLoginController::class, 'postLogin']);
         //Register
